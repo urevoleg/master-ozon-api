20dee6af832809143e77b0b4a62ad0986f85732c
urevoleg <urevolegg@gmail.com>
2023-11-25 20:05:26 +0300

    fix

--- course/chapter-2/compiled/dm_countries_and_genres.sql
+++ course/chapter-2/compiled/dm_countries_and_genres.sql
@@ -1,6 +1,5 @@
-{%- from 'reference.sql' import ref -%}
-{%- from 'config.sql' import materialization -%}
-{{materialization(type='view', schema='cdm', name='kp')}}
+DROP  VIEW IF EXISTS cdm.kp;
+CREATE  VIEW cdm.kp AS
 SELECT production_year,
        country,
        genre,
@@ -8,6 +7,6 @@ SELECT production_year,
        avg(imdb) as avg_imdb,
        avg(budget) as avg_budget,
        sum(box_office_usa) / sum(budget) - 1 as profit
-FROM {{ ref('raw_kp') }}
+FROM dds.raw_kp
 GROUP BY production_year, country, genre
 ORDER BY production_year
