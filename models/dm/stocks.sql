bce0554bbdb25f618a0d7cc87444e32697fc36cb
urevoleg <urevolegg@gmail.com>
2024-05-13 20:26:12 +0300

    fix dm.stocks

--- models/dm/stocks.sql
+++ models/dm/stocks.sql
@@ -1,27 +1,35 @@
 {{ config(
- tags=['dm_sku_stocks', 'sku_stocks', 'dm'],
+ tags=['dm_stocks', 'stocks', 'dm'],
  schema='dm',
  materialized='table',
 ) }}
 
-with rep as (SELECT
+with sat_product as (
+    select h.offer_id, -- артикул
+           h.product_pk,
+           s.product_name
+    from {{ ref('hub_products') }} h
+    join {{ ref('sat_products') }} s
+    on h.product_pk = s.product_pk
+),
+stock as (SELECT
     process_date - interval '1 day' as dated_at,
+    warehouse_pk,
     product_pk,
     delivery_type,
     warehouse_name,
     sum(free_to_sell_amount) as free_to_sell_amount,
     sum(reserved_amount) as reserved_amount
-    FROM {{ref('report_stocks')}}
-    group by dated_at, product_pk, delivery_type, warehouse_name)
-select rep.dated_at,
-       h.offer_id,
-       h.barcode,
+    FROM {{ ref('report_stocks') }}
+    group by dated_at, product_pk, delivery_type, warehouse_name, warehouse_pk)
+select stock.dated_at,
+       stock.warehouse_name,
+       s.offer_id,
        s.product_name,
-       rep.free_to_sell_amount,
-       rep.reserved_amount,
-       h.product_pk
-from {{ref('hub_products')}} h
-join {{ref('sat_products')}} s
-using (product_pk)
-join rep
+       stock.free_to_sell_amount,
+       stock.reserved_amount,
+       stock.warehouse_pk,
+       s.product_pk
+from sat_product s
+join stock
 using (product_pk)
